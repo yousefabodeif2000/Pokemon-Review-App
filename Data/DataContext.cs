90d4f59c627a56f3ba1db66dfbecd35dfaf1beee
yousefabodeif2000 <83887183+yousefabodeif2000@users.noreply.github.com>
2024-06-18 11:30:46 +0200

    Initial commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemon { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            /*JOINT TABLES CREATION
+             * Here, we are configuring the joint tables created in our UML diagram 
+             * */
+            modelBuilder.Entity<PokemonCategory>().HasKey(pc => new { pc.PokemonId, pc.CategoryId });
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(pc => pc.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Category)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(pc => pc.CategoryId);
+
+            modelBuilder.Entity<PokemonOwner>().HasKey(po => new { po.PokemonId, po.OwnerId });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(pc => pc.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(pc => pc.OwnerId);
+
+
+        }
+    }
+}
---
+++ Repository/PokemonRepository.cs
@@ -0,0 +1,19 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Models;
+using System.Data;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext dataContext) 
+        {
+            this._context = dataContext;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();        }
+    }
+}
